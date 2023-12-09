3b9c96b89c738254c56ac4111a270aa3417571c6
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-12-09 14:12:33 -0700

    basic registration page setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import Registration from './pages/Registration';
 
 const container = document.getElementById('app-root')!;
 const root = createRoot(container);
-root.render(<p>Hello React</p>);
+root.render(<Registration/>);
